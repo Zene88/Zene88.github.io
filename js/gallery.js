f72303603bdebb54ff842ac16c731673612c7df1
Zene88 <my_im@gmx.de>
2021-01-18 21:39:42 +0100

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,28 @@
+
+//Activates the image gallery
+//The main task is to attach an event listener to each image in the gallery
+//and respond appropriately on click
+
 function activateGallery() {
-    alert("Hello from the gallery file!");
+    let currentClass = "current";
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo img");
+    let description = document.querySelector("#gallery-info p");
+    let title = document.querySelector("#gallery-info h3");
+
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.addEventListener("click", function() {
+            //Set clicked image as main image
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            let newDescription = thumbnail.dataset.description;
+            let newTitle = thumbnail.dataset.title;
+            mainImage.setAttribute("src", newImageSrc);
+            description.innerHTML = newDescription;
+            title.innerHTML = newTitle;
+
+            //Change which image is current
+            document.querySelector("."+currentClass).classList.remove(currentClass);
+            thumbnail.parentNode.classList.add(currentClass);
+        });
+    });
 }
